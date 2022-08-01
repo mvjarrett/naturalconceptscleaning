36f35886b32ac7f1812b9acf5fdfd0e7792cfdb1
Matt <Mvjarrett92@gmail.com>
2022-08-01 15:12:24 -0400

    add bulma, begin layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { ImageHeroComponent } from './components/image-hero/image-hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TabsComponent,
+    ImageHeroComponent
   ],
   imports: [
     BrowserModule
